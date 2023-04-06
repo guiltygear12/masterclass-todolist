c6a541359d3fc17248bbd9b0dc2993995662b564
qkralsgud9 <mylove700757@gmail.com>
2023-04-06 17:35:08 +0900

    7.1 셀렉터 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { createGlobalStyle } from "styled-components";
-
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { createGlobalStyle } from 'styled-components';
+import { hourSelctor, minState } from './atoms';
 
 function App() {
+  const [min, setMin] = useRecoilState(minState);
+  const [hours, setHours] = useRecoilState(hourSelctor);
+  const onMinChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMin(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  };
   return (
     <div>
-      <h1>1</h1>
+      <input value={min} onChange={onMinChange} type="number" placeholder="Min" />
+      <input value={hours} onChange={onHoursChange} type="number" placeholder="Hour" />
     </div>
   );
 }
--- src/atoms.ts
+++ src/atoms.ts
@@ -1,1 +1,19 @@
-import { atom, selector } from "recoil";
+import { atom, selector } from 'recoil';
+
+export const minState = atom({
+  key: 'mins',
+  default: 0,
+});
+
+export const hourSelctor = selector<number>({
+  key: 'hours',
+  get: ({ get }) => {
+    const min = get(minState);
+    return min / 60;
+  },
+  set: ({ set }, newValue) => {
+    console.log(newValue);
+    const min = Number(newValue) * 60;
+    set(minState, min);
+  },
+});
